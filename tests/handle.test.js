d14f4b4a2a23725dd1b74e22ccabb8539f1e8ffe
Jonathan Hernandez <jbilbo@gmail.com>
2017-05-05 17:12:42 +0200

    Implement Handle extraction

---
+++ tests/handle.test.js
@@ -0,0 +1,22 @@
+/* globals test, expect */
+"use strict";
+
+const handle = require("../lib/handle");
+
+test("extracts a Handle", () => {
+    var text = "http://hdl.handle.net/10149/596901";
+
+    expect(handle.extract(text)).toEqual(["10149/596901"]);
+});
+
+test("extracts another Handle", () => {
+    var text = "http://hdl.handle.net/2117/83545it.ly/1UtXnTW";
+
+    expect(handle.extract(text)).toEqual(["2117/83545it.ly/1UtXnTW"]);
+});
+
+test("extracts Handles separated by Unicode whitespace", () => {
+    var text = "10149/596901 10251/79612";
+
+    expect(handle.extract(text)).toEqual(["10149/596901", "10251/79612"]);
+});
